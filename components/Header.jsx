11c35918799f0eb2a7ec8f7fbd0ad4c51c826eba
scottfully <83400381+scottfully@users.noreply.github.com>
2025-08-11 11:25:01 +1000

    Wrap header logo in a link back to the home

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,10 +3,13 @@ import trollFace from "../images/troll-face.png"
 export default function Header() {
     return (
         <header className="header">
-            <img 
-                src={trollFace} 
-            />
-            <h1>Meme Generator</h1>
+            <a href="/" className="header-link">
+                <img 
+                    src={trollFace} 
+                    alt="Meme Generator"
+                />
+                <h1>Meme Generator</h1>
+            </a>
         </header>
     )
 }
